6981fbb3c30193346c0628ee80fb6f590724eb7e
WavyZany <130682641+WavyZany@users.noreply.github.com>
2023-06-01 14:28:56 -0400

    Math Menu
    Main menu has been set up using a method
    Worked on creating char input to select an option easily
    Created additions.txt quiz option
    Imported images for use in game menu
    [*****] - brackets with stars represent priority (from 1 to 5 stars)

--- mathtraininggame.java
+++ mathtraininggame.java
@@ -1,23 +1,45 @@
 import arc.*;
 import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 public class mathtraininggame{
 	public static void main(String[] args){
 		Console con = new Console("Math Training Game", 1280, 720);
-		//mathMenu(con);
+		mathMenu(con);
 		
-		char charKey = con.getChar();
-		con.println(charKey); // make sure to follow variable names of User interface design
+		// make sure to follow variable names of User interface design
 		
 	}	// CUT OFF
 	
 	// MAIN MENU METHOD
 	public static void mathMenu(Console con){
-		con.setBackgroundColor(Color.WHITE);
-		con.setDrawColor(Color.GREEN);
-		con.fillRoundRect(500, 260, 300, 200, 70, 70);
-		con.setDrawColor(Color.BLACK);
-		con.drawString("PLAY", 500, 260);
+		BufferedImage imgBackground = con.loadImage("background.jpg");
+		con.drawImage(imgBackground, 0, 0);
+		BufferedImage imgLogo = con.loadImage("logo.png");
+		con.drawImage(imgLogo, 1045, 0);
+		con.repaint();
+		
+		con.println("PLAY (P)");
+		con.println("SCOREBOARD (S)");
+		con.println("HELP (H)");
+		char chrMenu = con.getChar();
+		
+		//  [**] put a loop if they do not choose any option, than it loops back until they choose one
+		if(chrMenu == 'p' || chrMenu == 'P'){
+			con.println("play");
+		}else if(chrMenu == 's' || chrMenu == 'S'){
+			con.println("scoreboard");
+		}else if(chrMenu == 'h' || chrMenu == 'H'){
+			// [*****] Finish help option as game goes on.
+			con.clear();
+			con.println("HELP:");
+			con.println("Try playing the game by pressing 'PLAY (p)'!");
+			con.sleep(1000);
+			con.clear();
+			mathMenu(con);
+		}
+		
+		
 		
 		
 		
